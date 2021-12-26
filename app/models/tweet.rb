c916693eb85d00af8d5a37b3db300ade9dd673e7
naramuratomofumi <tomofuminaramura@gmail.com>
2021-12-26 19:06:20 +0900

    テキストに加え、画像も空では投稿・編集できないように、バリデーションを追記

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,5 @@
 class Tweet < ApplicationRecord
-  validates :text, presence: true
+  validates :text, :image, presence: true   # imageも空で投稿できないように追記
   belongs_to :user
   has_many :comments  # commentsテーブルとのアソシエーション
-
 end
